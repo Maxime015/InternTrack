f6e78799995862c631434eb86c32b0b332131a0e
Maxime ANANIVI <maximeananivi@MacBook-Pro-de-Maxime.local>
2026-03-03 11:22:26 +0000

    hello

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -34,7 +34,7 @@ const Wrapper = ({ children }: WrapperProps) => {
     return (
         <div>
             <Navbar />
-            <div className="px-5 md:px-[10%] mt-8 mb-10">
+            <div className="px-5 md:px-[3.5%] mt-8 mb-10">
                 <ToastContainer
                     position="top-right"
                     autoClose={5000}
